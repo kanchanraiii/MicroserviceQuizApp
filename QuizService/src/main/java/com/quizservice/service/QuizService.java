4c46e81c6184e861df45decf5fa53e34b8c2222e
Kanchan Rai <kanchanrai2307@gmail.com>
2025-11-29 12:13:27 +0530

    added: circuit breaker

--- QuizService/src/main/java/com/quizservice/service/QuizService.java
+++ QuizService/src/main/java/com/quizservice/service/QuizService.java
@@ -5,6 +5,8 @@ import com.quizservice.model.QuestionWrapper;
 import com.quizservice.model.Quiz;
 import com.quizservice.model.Response;
 import com.quizservice.repository.QuizRepository;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,10 +24,20 @@ public class QuizService {
     private QuizInterface quizInterface;
 
    
+    @CircuitBreaker(
+            name = "questionServiceCB",
+            fallbackMethod = "createQuizFallback"
+    )
     public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
 
-        List<String> questionIds =
-                quizInterface.getQuestionsForQuiz(category, numQ).getBody();
+        ResponseEntity<List<String>> response =
+                quizInterface.getQuestionsForQuiz(category, numQ);
+
+        if (response == null || response.getBody() == null) {
+            throw new RuntimeException("No questions returned");
+        }
+
+        List<String> questionIds = response.getBody();
 
         Quiz quiz = new Quiz();
         quiz.setTitle(title);
@@ -36,7 +48,12 @@ public class QuizService {
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
 
+
    
+    @CircuitBreaker(
+    		name="questionServiceCB",
+    		fallbackMethod="getQuizQuestionsFallback"
+    )
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(String quizId) {
 
         Quiz quiz = quizRepo.findById(quizId).orElseThrow();
@@ -46,8 +63,35 @@ public class QuizService {
     }
 
    
+    @CircuitBreaker(
+    		name="questionServiceCB",
+    		fallbackMethod="calculateResultFallback")
     public ResponseEntity<Integer> calculateResult(String id, List<Response> responses) {
 
         return quizInterface.getScore(responses);
+    }
+    
+    
+    // adding the fallback methods
+    public ResponseEntity<String> createQuizFallback(String category, int numQ, String title, Throwable ex){
+    	
+    	System.out.println("Fallback - Create Quiz ");
+    	
+    	return new ResponseEntity<>(
+    			"Question service unavailable. Try again later.",
+                HttpStatus.SERVICE_UNAVAILABLE
+    	);
+    			
+    }
+    
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestionsFallback(String quizId, Throwable ex){
+    	return new ResponseEntity<>(List.of(), HttpStatus.SERVICE_UNAVAILABLE);
+    	
+    }
+    
+    public ResponseEntity<Integer> calculateResultFallback(String id, List<Response> responses, Throwable ex){
+    	return new ResponseEntity<>(
+                0, HttpStatus.SERVICE_UNAVAILABLE
+        );
     }
 }
